bf1bf52ae4810565c3d889916a215a18bb79dac6
vupn-1304 <vupn.1304@gmail.com>
2020-02-18 17:14:39 +0700

    Add Room, Pref

--- app/src/main/java/com/pv/demoapp/data/model/User.kt
+++ app/src/main/java/com/pv/demoapp/data/model/User.kt
@@ -1,13 +1,20 @@
 package com.pv.demoapp.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 
+@Entity(tableName = "user_info")
 data class User(
+        @PrimaryKey(autoGenerate = true)
+        @Expose var idLocal: Long? = null,
+
+        @Expose var id: String = "",
         @Expose var username: String = "",
         @Expose var email: String = "",
         @Expose var password: String = "",
-        @Expose var city: City? = null,
-        @Expose var district: District? = null,
-        @Expose var sex: Sex? = null,
+        @Expose var city: String? = null,
+        @Expose var district: String? = null,
+        @Expose var sex: String = "",
         @Expose var age: Int = 25
 )
--- app/src/main/java/com/pv/demoapp/di/app_module.kt
+++ app/src/main/java/com/pv/demoapp/di/app_module.kt
@@ -1,8 +1,14 @@
 package com.pv.demoapp.di
 
+import androidx.room.Room
 import com.google.gson.GsonBuilder
 import com.pv.demoapp.data.AppDataManager
 import com.pv.demoapp.data.DataManager
+import com.pv.demoapp.data.local.db.AppDatabase
+import com.pv.demoapp.data.local.db.AppDbHelper
+import com.pv.demoapp.data.local.db.DbHelper
+import com.pv.demoapp.data.local.prefs.AppPrefHelper
+import com.pv.demoapp.data.local.prefs.PrefHelper
 import com.pv.demoapp.data.remote.ApiHelper
 import com.pv.demoapp.data.remote.AppApiHelper
 import com.utils.SchedulerProvider
@@ -15,12 +21,20 @@ val appModule: Module = module {
 
     single { SchedulerProvider() }
 
-    single { AppDataManager(get()) as DataManager }
+    single { AppPrefHelper(get(), "prefsapp", get()) as PrefHelper }
+
+    single { AppDataManager(get(), get(), get()) as DataManager }
+
+    single { AppDbHelper(get()) as DbHelper }
 
     single { AppApiHelper() as ApiHelper }
 
     single {
         GsonBuilder().excludeFieldsWithoutExposeAnnotation().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'X'").create()!!
+    }
+
+    single {
+        Room.databaseBuilder(get(), AppDatabase::class.java, "databaseapp.sqlite").build()
     }
 }
 val demoModule = listOf(appModule, viewModule)
